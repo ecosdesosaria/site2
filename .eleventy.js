77a7a78706646973d9a24233ce39daf4763576a1
ecosdesosaria <desapontaeclica+sosaria@gmail.com>
2026-02-01 13:39:17 -0300

    Migrate site to Eleventy + add blog + layouts + pagination

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,36 @@
-module.exports = function(eleventyConfig) {
+export default function(eleventyConfig) {
 
-  // copy everything as-is (images, css, js, etc)
-  eleventyConfig.addPassthroughCopy("src");
+  // copy static files (images, icons, etc)
+  //eleventyConfig.addPassthroughCopy("src");
+
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("src/favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
+  eleventyConfig.addPassthroughCopy("src/logo_ecosdesosaria.png");
+  eleventyConfig.addPassthroughCopy("src/races");
+  eleventyConfig.addPassthroughCopy("src/destinos");
+
+  eleventyConfig.addFilter("readableDate", date => {
+    return new Date(date).toLocaleDateString("pt-BR", {
+      timeZone: "UTC"
+    });
+  });
+
+
+  // truncate filter (preview text)
+  eleventyConfig.addFilter("truncate", (str, len, end) => {
+    if (!str) return "";
+    if (str.length <= len) return str;
+    return str.substring(0, len) + end;
+  });
+
+  // date format
+  //eleventyConfig.addFilter("readableDate", date => {
+  //  return new Date(date).toLocaleDateString("pt-BR");
+  //});
 
   return {
     dir: {
@@ -9,4 +38,4 @@ module.exports = function(eleventyConfig) {
       output: "dist"
     }
   };
-};
+}
---
+++ src/blog/ecos-de-sosaria-blog.md
@@ -0,0 +1,21 @@
+---
+title: Bem-Vindos!
+date: 2026-02-01
+layout: post.njk
+tags: blog
+description: Bem-vindos ao blog de Ecos de Sosaria
+---
+
+# Bem-vindos aventureiros 🏰
+
+Hoje inauguramos o **blog oficial de Ecos de Sosaria**.
+
+Aqui vamos compartilhar:
+
+- novidades do shard
+- updates
+- eventos
+- histórias de roleplay
+- bastidores do desenvolvimento
+
+Nos vemos em Sosaria ⚔️
